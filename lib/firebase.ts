6ae302750efa618f8a1298833d9b583392156539
Lye-0 <kawauso.pp.s15@gmail.com>
2026-06-14 00:18:46 +0900

    FirebaseのAPIキーを環境変数化

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDKX9y3k9uyPU_qHBq7X46lUnkEE-0Cbx4",
-  authDomain: "chess-64d14.firebaseapp.com",
-  projectId: "chess-64d14",
-  storageBucket: "chess-64d14.firebasestorage.app",
-  messagingSenderId: "423879495214",
-  appId: "1:423879495214:web:09480639d381b4b2b619bf"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
