c6b4994d34ee0b730e5471a1ea6c213981b314ee
Lye-0 <kawauso.pp.s15@gmail.com>
2026-06-13 16:10:40 +0900

    firebase接続完了

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKX9y3k9uyPU_qHBq7X46lUnkEE-0Cbx4",
+  authDomain: "chess-64d14.firebaseapp.com",
+  projectId: "chess-64d14",
+  storageBucket: "chess-64d14.firebasestorage.app",
+  messagingSenderId: "423879495214",
+  appId: "1:423879495214:web:09480639d381b4b2b619bf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
